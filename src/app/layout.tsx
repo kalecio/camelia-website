7cf6e03ebb649d44cf3a54010ddf3a96538ce54b
Kalécio Pereira <kalecio.santana@gmail.com>
2025-09-25 01:29:12 -0300

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Camélia Saboaria Artesanal",
   description: "Camélia Saboaria Artesanal",
+  openGraph: {
+    type: "website",
+    countryName: "Brazil",
+    locale: "pt-BR",
+    title: "Camélia Saboaria Artesanal",
+    description: "Sabonetes e cuidados naturais feitos à mão, unindo beleza, bem-estar e respeito ao meio ambiente.",
+    images: [
+      {
+        url: "/images/og-image.jpg",
+        width: 1800,
+        height: 1800,
+        alt: "Camélia Saboaria Artesanal - Sabonetes Naturais"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
